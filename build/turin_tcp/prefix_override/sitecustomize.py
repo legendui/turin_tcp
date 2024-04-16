c2cfbbce2a76bfeb627464ed933e040a15bf8e96
legendui <ruichen99@hotmail.com>
2024-04-16 17:22:01 +0800

    重构

---
+++ build/turin_tcp/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/rui/ros2_ws/src/turin_tcp/install/turin_tcp'
---
+++ turin_tcp/turin_tcp/README.md
@@ -0,0 +1,2 @@
+# turin_tcp
+ a ros2 package tool box for turin robot
